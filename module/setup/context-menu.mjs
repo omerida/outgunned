6ba4d3be7b5cece3c5135b3af9c905895ca57f45
Oscar Merida <oscar@oscarm.org>
2026-03-14 00:12:54 -0400

    Silence a deprecation warning by setting the jQuery option to true. Added a comment that this will be deprecated in v15

--- module/setup/context-menu.mjs
+++ module/setup/context-menu.mjs
@@ -1,11 +1,15 @@
 export class OutgunnedContextMenu extends foundry.applications.ux.ContextMenu.implementation
 {
   constructor(element, selector, menuItems, options = {}) {
+    // @TODO: Deprecated in v15
+    options.jQuery ??= true;
     super(element, selector, menuItems, options);
   }
   
     /** @inheritdoc */
   static create(app, element, selector, items, { hookName = "EntryContext", ...options } = {}) {
+    // @TODO: eprecated in v15
+    options.jQuery ??= true;
     options.fixed ??= true;
     app._callHooks?.(className => `get${className}${hookName}`, items);
     return new this(element, selector, items, options);
